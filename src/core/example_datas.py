9d68ec638f5381f76f6dbed766492356d7e141d1
Ludovic SANSONE <libreaudio63@gmail.com>
2022-04-27 08:39:26 +0200

    Ajout du fichier exampleèdatas.py, contenant des données de test pour le programme

---
+++ src/core/example_datas.py
@@ -0,0 +1,33 @@
+tournaments_table = [
+    {
+        'id': '1',
+        'name': 'Grand Prix d\'Auvergne',
+        'place': 'Maison de quartier Saint Jacques - Clermont-Ferrand',
+        'dates': ['26/04/2022', '29/04/2022']
+    },
+    {
+        'id': '2',
+        'name': 'Tournoi annuel des 12 18 ans',
+        'place': 'Centre culturel Marcel Pagnol - Martigues',
+        'dates': ['30/04/2022', '30/04/2022']
+    },
+]
+
+players_table = [
+    {
+        'id': '1',
+        'firstname': 'Delphine',
+        'lastname': 'Marcoccio',
+        'sexe': 'Femme',
+        'birthday': '15/12/1976',
+        'ranking': 'Non Classé',
+    },
+    {
+        'id': '2',
+        'firstname': 'Ludovic',
+        'lastname': 'Sansone',
+        'sexe': 'Homme',
+        'birthday': '19/03/1984',
+        'ranking': 'Non Classé',
+    },
+]
