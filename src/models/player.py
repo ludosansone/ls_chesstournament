c0b1935227a8a0b6101b1403acaa2398b802b8ca
Ludovic SANSONE <libreaudio63@gmail.com>
2022-04-27 08:37:14 +0200

    Ajout des fichiers player.py et tournament.py, contenant les classes du modèle du programme

---
+++ src/models/player.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(
+            self,
+            firstname="",
+            lastname="",
+            birthday="",
+            sexe="",
+            ranking=0):
+
+        self.firstname = firstname
+        self.lastname = lastname
+        self.birthday = birthday
+        self.sexe = sexe
+        self.ranking = ranking
+
+    def add_player(self):
+        pass
---
+++ src/models/tournament.py
@@ -0,0 +1,23 @@
+class Tournament:
+    def __init__(
+            self,
+            name="",
+            place="",
+            dates=[],
+            rounds_number=4,
+            rounds=[],
+            players=[],
+            time_control="",
+            description=""):
+
+        self.name = name
+        self.place = place
+        self.dates = dates
+        self.rounds_number, = rounds_number,
+        self.rounds = rounds
+        self.players = players
+        self.time_control = time_control
+        self.description = description
+
+    def get_name(self):
+        return self.name
