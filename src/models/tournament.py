8e439f9754784211eef7e554cc9f66eb48feeabd
Ludovic SANSONE <libreaudio63@gmail.com>
2022-05-13 21:16:43 +0200

    Déplacement de certaines parties du code de la classe Tournament vers la classe TournamentDelegate

--- src/models/tournament.py
+++ src/models/tournament.py
@@ -2,6 +2,7 @@ from functools import cmp_to_key
 from tinydb import TinyDB, Query
 from models.player import Player
 from models.round import Round
+from models.tournamentdelegate import TournamentDelegate
 
 
 class Tournament:
@@ -82,11 +83,12 @@ class Tournament:
         # On trie les joueurs selon leur classement général
         instance_list_players.sort(key=lambda p: p.ranking)
 
-        # On met à jour le classement du tournoi
+        # On réorganise la liste des joueurs du tournoi, en fonction du classement général
         self.players = []
 
         for player in instance_list_players:
             self.players.append(player.id)
+
         return instance_list_players
 
     def get_first_round_players(self):
@@ -94,17 +96,10 @@ class Tournament:
             consttitution des paires de joueurs pour le premier tour du tournoi
         """
 
-        first_round_players = []
-        i = 0
+        # On récupère la liste des dictionnaires de joueurs, ordonnée pour le premier tour
+        first_round_players = TournamentDelegate.get_first_peers(self)
 
-        while i < 4:
-            player1 = self.players[i]
-            player2 = self.players[i + 4]
-            first_round_players.append(player1)
-            first_round_players.append(player2)
-            i += 1
-
-        # On récupère la liste des instances de joueurs, prêts à disputer le premier tour
+        # On récupère la liste des instances de joueurs, à partir de la liste de dictionnaires récupérée ci-dessus
         instance_first_round_players = Player.get_tournament_players(first_round_players)
 
         # On réorganise la liste des joueurs du tournoi, en fonction des paires de joueurs constituées
@@ -120,47 +115,14 @@ class Tournament:
             constitution des paires de joueurs pour les autres rounds du tournoi
         """
 
-        round_index = 0
-
         # On récupère les tours du tournoi
         rounds = Round.get_tournament_rounds(self.rounds)
 
         # On récupère les joueurs avec leur score des tours précédents, dans une liste de dictionnaires
-        list_players = []
-
-        while round_index < len(rounds):
-            match_number = 0
-            while match_number < 4:
-                player1_id = rounds[round_index].matchs[match_number][0][0]
-                player1_score = float(rounds[round_index].matchs[match_number][0][1])
-                player1 = Player.read(player1_id)
-                player1_ranking = player1.ranking
-                player2_id = rounds[round_index].matchs[match_number][1][0]
-                player2_score = float(rounds[round_index].matchs[match_number][1][1])
-                player2 = Player.read(player2_id)
-                player2_ranking = player2.ranking
-                player_found = False
-                player_index = 0
-                while player_index < len(list_players):
-                    if list_players[player_index]['player_id'] == player1_id:
-                        new_score = float(list_players[player_index]['score']) + player1_score
-                        player = {'player_id': player1_id, 'score': str(new_score), 'ranking': player1_ranking}
-                        list_players[player_index] = player
-                        player_found = True
-                    elif list_players[player_index]['player_id'] == player2_id:
-                        new_score = float(list_players[player_index]['score']) + player2_score
-                        player = {'player_id': player2_id, 'score': str(new_score), 'ranking': player2_ranking}
-                        list_players[player_index] = player
-                        player_found = True
-                    player_index += 1
-                if player_found is False and round_index == 0:
-                    list_players.append({'player_id': player1_id, 'score': player1_score, 'ranking': player1_ranking})
-                    list_players.append({'player_id': player2_id, 'score': player2_score, 'ranking': player2_ranking})
-                match_number += 1
-            round_index += 1
+        list_players = TournamentDelegate.get_all_scores(rounds)
 
         # On trie les joueurs selon leur nombre de points, ou selon leur rang en cas d'égalité
-        list_players.sort(key=cmp_to_key(Tournament.compare))
+        list_players.sort(key=cmp_to_key(TournamentDelegate.compare))
 
         # On réorganise la liste des joueurs du tournoi, avec les paires nouvellement constituées
         self.players = []
@@ -243,20 +205,3 @@ class Tournament:
         query = Query()
         tournament_number = len(db.search(query.type == 'tournament'))
         return tournament_number
-
-    def compare(p1, p2):
-        """
-            Fonction auxilière, permettant la constitution des paires de joueurs à partir du second tour
-        """
-
-        if p1['score'] > p2['score']:
-            return -1
-        elif p1['score'] < p2['score']:
-            return 1
-        else:
-            if p1['ranking'] > p2['ranking']:
-                return 1
-            elif p1['ranking'] < p2['ranking']:
-                return -1
-            else:
-                return 0
